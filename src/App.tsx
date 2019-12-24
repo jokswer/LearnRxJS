42eef0d8d4b04fa5a1b08535f2f25fcf28475b50
jokswer <ramil@Akbirovs-Mac-mini.local>
2019-12-24 11:24:47 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { fromEvent } from "rxjs";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
+  let [clicks, setClick] = React.useState(0);
+  let [x, setX] = React.useState(0);
+  let [y, setY] = React.useState(0);
+
+  const click$ = fromEvent<MouseEvent>(document, "click").subscribe(event =>
+    setClick((clicks += 1))
+  );
+
+  const mouse$ = fromEvent<MouseEvent>(document, "mousemove").subscribe(
+    event => {
+      setX(event.x);
+      setY(event.y);
+    }
+  );
+
+  click$.add();
+  mouse$.add();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          X: {x} Y: {y}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{ fontSize: 36 }}>{clicks}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
